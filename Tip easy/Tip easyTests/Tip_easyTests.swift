fb1fc5a0801dc1d59a5e5bcff0a101d6ec27850d
Raynardfreeman <60630748+Raynardfreeman@users.noreply.github.com>
2020-02-03 18:50:27 -0500

    Add files via upload

---
+++ Tip easy/Tip easyTests/Tip_easyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tip_easyTests.swift
+//  Tip easyTests
+//
+//  Created by Briana Williams on 12/13/19.
+//  Copyright © 2019 Briana Williams. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_easy
+
+class Tip_easyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
